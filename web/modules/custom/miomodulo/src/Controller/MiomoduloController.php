426be2ab6f2296444d1f4b9ab669573d77f268e7
Nicola Dimatteo <606982+webnicola@users.noreply.github.com>
2025-11-12 11:30:41 +0100

    aggiuna pagina custom (route controller)

---
+++ web/modules/custom/miomodulo/src/Controller/MiomoduloController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\miomodulo\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class MiomoduloController extends ControllerBase {
+
+  /**
+   *
+   */
+  function miaPagina() {
+  $miavar['test'] = [
+    'nomee' => 'nicola',
+  ];
+  $nome = $miavar['test']['nome'] ?? 'no nome';
+  dump($nome);
+  $msg = 'Render html Mia pagina';
+
+  return [
+    '#markup' => '<h2>' . $this->t($msg) . '</h2>',
+  ];
+ }
+
+}
